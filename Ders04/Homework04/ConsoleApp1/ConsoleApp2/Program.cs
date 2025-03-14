5c8bcf108711a2066718a212d2f9109e267fb3b9
ahmet <ahmetrasimbayhan@gmail.com>
2025-03-14 21:26:03 +0300

    Net React Ödev - Şubat 1

---
+++ Ders04/Homework04/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Diziler (Arrays), aynı türde birden fazla veriyi tek bir değişkende saklamamızı sağlar.  
+// Dizilere, her elemanın belirli bir indeks (index) numarası ile erişilir.  
+// Örneğin, birden fazla araba markasını farklı değişkenlerde tutmak yerine,  
+// hepsini tek bir dizi içinde saklayarak daha düzenli ve yönetilebilir hale getirebiliriz.
+
+
+Console.WriteLine("Lütfen 5 adet tam sayı giriniz:");
+
+int[] dizi = new int[5];
+
+for (int i = 0; i < dizi.Length; i++)
+{
+  Console.Write($"Sayı {i + 1}: ");
+  dizi[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("\nGirilen sayılar:");
+foreach (var item in dizi)
+{
+  Console.WriteLine(item);
+}
+
+int max = dizi[0];
+for (int i = 1; i < dizi.Length; i++)
+{
+  if (dizi[i] > max)
+  {
+    max = dizi[i];
+  }
+}
+
+Console.WriteLine($"\nEn büyük sayı: {max}");
