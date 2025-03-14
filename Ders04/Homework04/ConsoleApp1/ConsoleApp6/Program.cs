a205ba4bde546eaf301dda1002e6e9c617c7572e
ahmet <ahmetrasimbayhan@gmail.com>
2025-03-14 22:24:05 +0300

    Net React Ödev - Ocak 1

---
+++ Ders04/Homework04/ConsoleApp1/ConsoleApp6/Program.cs
@@ -0,0 +1,12 @@
+﻿// C# programlama dili bir high level programlama dilidir, oyun web masaüstü embedded gibi türlü
+// türlü alanlarda kullanılır
+// Bir programın çalışması için temel bileşenler kodu derleyecek olan idedir,
+// programlama dilini bilgisayarın anlayacağı low level makina iline çevirir.
+// Derleme sürecinde ram ve cpu yu kullanır
+
+Console.WriteLine("Lutfen adinizi giriniz");
+string isim = Console.ReadLine();
+Console.WriteLine(isim);
+
+
+
