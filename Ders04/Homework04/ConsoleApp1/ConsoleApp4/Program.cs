ce5a0336c2e16087bce52fbdce86a2625be21543
ahmet <ahmetrasimbayhan@gmail.com>
2025-03-14 22:12:37 +0300

    Net React Ödev - Ocak 3

---
+++ Ders04/Homework04/ConsoleApp1/ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿// İf else belirli bir ifade çalışmadığında olacak başka koşulları göstermek ve
+// hangi koşulda ne yapacağını tanımlamak için kullanılan bir yapıdır.
+Console.WriteLine("Sayi giriniz");
+int sayi = int.Parse(Console.ReadLine());
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Sayi cifttir");
+}
+else
+{
+    Console.WriteLine("Sayi tektir");
+}
+
+
+Console.WriteLine("Sayi giriniz");
+int sayi2 = int.Parse(Console.ReadLine());
+
+if (sayi2 == 0)
+{
+    Console.WriteLine("Sayi 0 dir");
+}
+else if(sayi2 > 0)
+{
+    Console.WriteLine("Sayi Pozitif");
+}
+else if (sayi2 < 0)
+{
+    Console.WriteLine("Sayi negatiftir");
+}
