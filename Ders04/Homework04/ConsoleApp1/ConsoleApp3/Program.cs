1c0dfa7e91fd43ded0190a95cdd3e6adb1bdeb16
ahmet <ahmetrasimbayhan@gmail.com>
2025-03-14 21:47:17 +0300

    Net React Ödev - Ocak 4

---
+++ Ders04/Homework04/ConsoleApp1/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Döngüler mantıksal koşul veya koşullar karşılandığı sürece kendini yineleyen yapılardır
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+        
+}
+
+Console.WriteLine("Sayi giriniz");
+int toplam = 0;
+int sayi = int.Parse(Console.ReadLine());
+for (int i = 0; i <= sayi; i++)
+{
+    toplam += i;
+}
+
+Console.WriteLine(toplam);
