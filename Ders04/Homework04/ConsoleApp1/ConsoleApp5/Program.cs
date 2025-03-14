8937f2bfcdffc32ac09cfe820e6bdfbb7cb3f303
ahmet <ahmetrasimbayhan@gmail.com>
2025-03-14 22:18:27 +0300

    Net React Ödev - Ocak 2

---
+++ Ders04/Homework04/ConsoleApp1/ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Temel veri tipleri int tam sayı double ve float ondalıklı sayı
+// string karakter dizisi char karakterdir
+//
+// int x = 5;
+// char a = 'b';
+// string b = "selam";
+// double tets = 3.4;
+// float se = 3;
+
+
+// int x = 5;  4 byte 
+// double y = 5.2; 8 byte  
+// string name = "Mehmet"; 2x6 byte 
+ 
+Console.WriteLine("2 Sayi girin toplayalim");
+int sayi1 = Convert.ToInt32(Console.ReadLine());
+int sayi2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sayi1 + sayi2);
