7ae0a30bf3a53a6c9d41b30577849c1de9bf83cb
Ahmet Rasim Beyhan <ahmetrasimbayhan@gmail.com>
2025-04-20 13:16:54 +0300

    Nisan 2 Odev Added

---
+++ Nisan_2_Odev/Nisan_2_Odev/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nisan_2_Odev.Data;
+using Nisan_2_Odev.Models;
+
+namespace Nisan_2_Odev.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly ProductRepository _repository;
+
+    public ProductController(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        _repository = new ProductRepository(connectionString);
+    }
+
+    [HttpGet]
+    public IActionResult GetAllProducts()
+    {
+        var products = _repository.GetAllProducts();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProductById(int id)
+    {
+        var product = _repository.GetProductById(id);
+        if (product == null)
+            return NotFound();
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public IActionResult AddProduct([FromBody] Product product)
+    {
+        _repository.AddProduct(product);
+        return CreatedAtAction(nameof(GetProductById), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateProduct(int id, [FromBody] Product product)
+    {
+        var existingProduct = _repository.GetProductById(id);
+        if (existingProduct == null)
+            return NotFound();
+
+        _repository.UpdateProduct(id, product);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var existingProduct = _repository.GetProductById(id);
+        if (existingProduct == null)
+            return NotFound();
+
+        _repository.DeleteProduct(id);
+        return NoContent();
+    }
+}
---
+++ Nisan_2_Odev/Nisan_2_Odev/Data/ProductRepository.cs
@@ -0,0 +1,101 @@
+﻿using Nisan_2_Odev.Models;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+namespace Nisan_2_Odev.Data;
+
+public class ProductRepository
+{
+    private readonly string _connectionString;
+
+    public ProductRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        var products = new List<Product>();
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("SELECT * FROM Products", connection);
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    products.Add(new Product
+                    {
+                        Id = (int)reader["Id"],
+                        Name = reader["Name"].ToString(),
+                        Price = (decimal)reader["Price"],
+                        Stock = (int)reader["Stock"]
+                    });
+                }
+            }
+        }
+        return products;
+    }
+
+    public Product GetProductById(int id)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("SELECT * FROM Products WHERE Id = @Id", connection);
+            command.Parameters.AddWithValue("@Id", id);
+            using (var reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    return new Product
+                    {
+                        Id = (int)reader["Id"],
+                        Name = reader["Name"].ToString(),
+                        Price = (decimal)reader["Price"],
+                        Stock = (int)reader["Stock"]
+                    };
+                }
+            }
+        }
+        return null;
+    }
+
+    public void AddProduct(Product product)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("INSERT INTO Products (Name, Price, Stock) VALUES (@Name, @Price, @Stock)", connection);
+            command.Parameters.AddWithValue("@Name", product.Name);
+            command.Parameters.AddWithValue("@Price", product.Price);
+            command.Parameters.AddWithValue("@Stock", product.Stock);
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public void UpdateProduct(int id, Product product)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("UPDATE Products SET Name = @Name, Price = @Price, Stock = @Stock WHERE Id = @Id", connection);
+            command.Parameters.AddWithValue("@Id", id);
+            command.Parameters.AddWithValue("@Name", product.Name);
+            command.Parameters.AddWithValue("@Price", product.Price);
+            command.Parameters.AddWithValue("@Stock", product.Stock);
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public void DeleteProduct(int id)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("DELETE FROM Products WHERE Id = @Id", connection);
+            command.Parameters.AddWithValue("@Id", id);
+            command.ExecuteNonQuery();
+        }
+    }
+}
